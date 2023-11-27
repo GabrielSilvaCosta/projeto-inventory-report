ac4793f17154ea6d802e83b9840c73d9d4ce4adc
GabrielSilvaCosta <bob@>
2023-11-27 18:48:32 -0300

    requisito 5 feito

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from typing import List
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: List[Product] = None):
+        self._data = data or []
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data.copy()
+
+    def add_data(self, products: List[Product]) -> None:
+        self._data.extend(products)
