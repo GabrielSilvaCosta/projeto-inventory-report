b90a8fa49cefbd9cd9246610ba716a76ff0097f9
GabrielSilvaCosta <bob@>
2023-11-27 17:18:40 -0300

    requisito 1 feito

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,27 @@
-def test_create_product() -> None:
-    raise NotImplementedError
+from inventory_report.product import Product
+
+
+def test_create_product():
+    product = Product(
+        id="123",
+        company_name="Company A",
+        product_name="Product X",
+        manufacturing_date="2022-01-01",
+        expiration_date="2023-01-01",
+        serial_number="SN123",
+        storage_instructions="Store in a cool place",
+    )
+
+    assert product.id == "123"
+
+    assert product.company_name == "Company A"
+
+    assert product.product_name == "Product X"
+
+    assert product.manufacturing_date == "2022-01-01"
+
+    assert product.expiration_date == "2023-01-01"
+
+    assert product.serial_number == "SN123"
+
+    assert product.storage_instructions == "Store in a cool place"
